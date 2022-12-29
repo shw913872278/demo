3d31f47280e5c752d9f193a6ae10cf88fdf9fa16
shw <913872278@qq.com>
2022-12-29 20:35:06 +0800

    version-1.3.8(hot-fix)

--- src/main/java/cn/study/controller/IndexController.java
+++ src/main/java/cn/study/controller/IndexController.java
@@ -2,11 +2,14 @@ package cn.study.controller;
 
 import cn.study.entity.User;
 import cn.study.service.IUserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Objects;
 
 @Controller
 public class IndexController {
@@ -17,8 +20,12 @@ public class IndexController {
     }
 
     @GetMapping({"/", "/index"})
-    public String index() {
+    public String index(HttpSession session) {
         final User login = userService.login(new User());
+        if (Objects.nonNull(login)) {
+            //TODO: 这里需要将登录信息存入session中
+            session.setAttribute("loginInfo", login);
+        }
         System.out.println("当前登录信息为：login = " + login);
         return "";
     }
