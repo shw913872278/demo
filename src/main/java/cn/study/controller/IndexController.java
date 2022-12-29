d393b159d9862d0f9bfe0c3ac030b11bcfa97219
shw <913872278@qq.com>
2022-12-29 19:29:42 +0800

    version-1.0.1

--- src/main/java/cn/study/controller/IndexController.java
+++ src/main/java/cn/study/controller/IndexController.java
@@ -1,10 +1,18 @@
 package cn.study.controller;
 
+import cn.study.service.IUserService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    private IUserService userService;
+
+    public IndexController(@Qualifier("userService") IUserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping({"/", "/index"})
     public String index() {
         return "";
---
+++ src/main/java/cn/study/service/impl/UserService.java
@@ -0,0 +1,8 @@
+package cn.study.service.impl;
+
+import cn.study.service.IUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+}
